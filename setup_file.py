61967550a88346ea7278f518798a720861de30e5
Appúchia <46938448+appuchias@users.noreply.github.com>
2020-06-23 12:37:25 +0200

    User friendlier

--- setup_file.py
+++ setup_file.py
@@ -1,25 +1,25 @@
 import json
 
 try:
-    with open("credentials.json", "r+") as r:
+    with open("credenciales.json", "r+") as r:
         if len(r.read()) > 2:
-            print("Credentials file already exists. Any change will overwrite the previous file")
+            print("El archivo con las credenciales ya existe. Los cambios sobreescribirán lo guardado anteriormente")
         else:
             r.write("{}")
 except:
-    file = open("credentials.json", "w")
+    file = open("credenciales.json", "w")
     file.write("{}")
     file.close()
 
-with open("credentials.json") as r:
+with open("credenciales.json") as r:
     credentials = json.load(r)
 
 credentials["login_username"] = input(
-    "Please input the username used to log into the bot account:\n> ")
+    "Nombre de usuario de la cuenta secundaria:\n> ")
 credentials["login_password"] = input(
-    "Please input the password used to log into the bot account:\n> ")
+    "Contraseña de la cuenta secundaria:\n> ")
 credentials["username"] = input(
-    "Please input the username to track followers from:\n> ")
+    "Cuenta de la que revisar los seguidores:\n> ")
 
-with open("credentials.json", "w") as w:
+with open("credenciales.json", "w") as w:
     json.dump(credentials, w, indent=4)
