397b1c2f47f24dad3680d5b5dff24067ec81670e
Appuchia <46938448+appuchias@users.noreply.github.com>
2020-09-15 23:46:21 +0200

    Should work now. Any bugs found will be solved shortly

--- main.py
+++ main.py
@@ -1,99 +1,153 @@
+import json
+import os
 from time import sleep
 from random import randint
 from datetime import datetime
 from igramscraper.instagram import Instagram
 from igramscraper.exception.instagram_auth_exception import InstagramAuthException
 
-from os import getenv
+from os import getenv  # pylint: disable-msg=C0411
 from dotenv import load_dotenv
 
 load_dotenv()
 
-DELAY = 40  # Cooldown between follower checks in minutes
+DELAY = 25  # Cooldown between follower checks in minutes
 
 # Log in function
-def login(username):
+def login():
     ig = Instagram()
     ig.with_credentials(getenv("LOGIN"), getenv("PASSWORD"))
     ig.login()
 
-    print("Logged in")
-
-    sleep(randint(0, 10))
-
-    account = ig.get_account(username)
-
-    return ig, account
+    return ig
 
 
 # Get followers
 def get_followers(ig, account):
     """Get all followers from an account"""
 
+    count = (
+        int(account.followed_by_count / 100) + (account.followed_by_count % 100 > 0)
+    ) * 100
+    print(
+        "    Estimated "
+        + str(count)
+        + " followers (~"
+        + str(round(count / 10 / 60, 1))
+        + " minutes)"
+    )
+
     try:
-        followers = ig.get_followers(account.identifier, 2000, 150, delayed=True)[
-            "accounts"
-        ]
+        followers = ig.get_followers(account.identifier, count, 150, delayed=True)
+        followers = followers["accounts"]
     except InstagramAuthException:
-        print("Error. Rate limited. Waiting before retrying...")
-        raise SystemExit("Exiting the program...")
+        raise SystemExit("Rate limited. Exiting the program...")
 
     return followers
 
 
 # Checker function
-def main():
+def main(ig, account):
     """Main function"""
 
-    ig, account = login(getenv("USERNAME"))
-
-    print("Getting followers")
+    print("  Getting followers")
 
     followers = get_followers(ig, account)
     followers_usernames = [follower.username for follower in followers]
 
-    print(str(len(followers_usernames)) + " followers")
+    try:
+        with open("followers.json", "r") as r:
+            users = json.load(r)
+    except FileNotFoundError:
+        users = {"": ["", ""]}
 
     try:
-        with open("followers.txt", "r") as r:
-            users = r.readlines()
+        with open("unfollowers.json", "r") as r2:
+            unfollowers = json.load(r2)
     except FileNotFoundError:
-        users = []
+        unfollowers = {"": {"": ["", ""]}}
 
-    unfollowers = []
+    # Fill both dictionaries
+    try:
+        users[account.username]
+    except KeyError:
+        users[account.username] = []
+    try:
+        unfollowers[account.username]
+    except KeyError:
+        unfollowers[account.username] = {}
 
-    print("Getting unfollowers:")
+    # Print followers
+    new_followers = []
 
-    if users:
-        for user in users:
-            if user.strip() not in followers_usernames:
-                unfollowers.append(user)
-                print("  Unfollower: @", user)
-    else:
-        print("No followers saved. Unfollowers will appear on next check")
+    for follower in followers_usernames:
+        if follower not in [user.strip() for user in users[account.username]]:
+            new_followers.append(follower)
 
-    with open("followers.txt", "w") as w:
-        w.write("\n".join(followers_usernames))
+    print(
+        "    Found "
+        + str(len(followers_usernames))
+        + " followers -> ±"
+        + str(len(new_followers))
+        + ((" -> [" + " ".join(new_followers) + "]") if new_followers else "")
+    )
 
-    with open("unfollowers.txt", "a") as a:
-        if unfollowers:
-            a.write(str(datetime.now()))
-        a.write("\n".join(unfollowers) + "\n")
+    # Get unfollowers
+    for user in users[account.username]:
+        if user.strip() not in followers_usernames:
+            current_time = str(datetime.now())
+            unfollowers[account.username][current_time] = []
+            unfollowers[account.username][current_time].append(user)
+            print("    Unfollower -> @" + user + " [@" + account.username + "]")
+
+    # Write save files
+    with open("followers.json", "w") as w:
+        users[account.username] = followers_usernames
+        json.dump(users, w, indent=4)
+
+    with open("unfollowers.json", "w") as w2:
+        json.dump(unfollowers, w2, indent=4)
 
 
 # Create checker loop
 def loop():
     """Just the checker loop function"""
 
+    check = 0
+
+    print("Starting up...")
+    ig = login()
+
+    sleep(randint(1, 10))
+    os.system("cls" if os.name == "nt" else "clear")
+
     while True:
+
+        check += 1
+
         try:
-            main()
-            true_delay = randint(DELAY - 15, DELAY + 15)
-            print(str(datetime.now()) + " -> Current delay: " + str(true_delay) + "min")
-            print("---------------------")
-            sleep(true_delay * 60)
+
+            with open("usernames.txt", "r") as r:
+                usernames = r.readlines()
+
+            print("Check #" + str(check))
+            print("-" * 26)  # ----------
+            print(str(datetime.now()) + "\n")
+
+            for username in usernames:
+                print("@" + username.strip())
+                account = ig.get_account(username.strip())
+                main(ig, account)
+                print("*-* " * 6)  # *-* *-* *-*
+
+            true_delay = randint(DELAY - 10, DELAY + 10)
+            print("Delay: " + str(true_delay) + "min\n")
+            sleep(true_delay * 60)  # Transfer to seconds
+
         except KeyboardInterrupt:
             raise SystemExit("\n\n  --Exited--\n\n")
+        except InstagramAuthException:
+            raise SystemExit("\n\n  --Instagram error--\n\n")
 
 
 if __name__ == "__main__":
